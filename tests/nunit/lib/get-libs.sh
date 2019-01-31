6de52dba40e4053e56ca75befdb9df227dc07946
CompulsiveCoder <compulsivecoder@gmail.com>
2019-01-31 05:46:27 +0000

    Added missing test libs scripts

---
+++ tests/nunit/lib/get-libs.sh
@@ -0,0 +1,15 @@
+echo "Getting library files..."
+echo "  Dir: $PWD"
+
+
+# Nuget is disabled because it's not currently required
+#sh get-nuget.sh
+#sh nuget-update-self.sh
+
+sh nuget-install.sh NUnit 2.6.4
+sh nuget-install.sh NUnit.Runners 2.6.4
+sh nuget-install.sh Newtonsoft.Json 11.0.2
+sh nuget-install.sh ArduinoSerialControllerClient 1.0.9
+sh nuget-install.sh duinocom.core 1.0.6
+
+echo "Finished getting library files."
