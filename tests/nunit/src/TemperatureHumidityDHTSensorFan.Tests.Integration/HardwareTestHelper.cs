79b172377ed43c3faa39fc16eadaaac2af18d9f7
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-27 07:50:39 +0000

    Fixed reset issue

--- tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/HardwareTestHelper.cs
+++ tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/HardwareTestHelper.cs
@@ -196,9 +196,8 @@ namespace TemperatureHumidityDHTSensorFan.Tests.Integration
 			// Give the pin some time at LOW to ensure reset
 			Thread.Sleep (10);
 
-			// Change the reset trigger pin to an input to go back to normal
-			// TODO: Implement a cleaner way to do this
-			SimulatorClient.DigitalRead (ResetTriggerPin);
+			// Cancel the reset
+      SimulatorClient.PinMode (ResetTriggerPin, PinMode.INPUT_PULLUP);
 
 			// Re-open the connection to the device
 			ConnectDevice ();
