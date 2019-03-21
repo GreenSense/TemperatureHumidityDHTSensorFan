baf8f0d96805524dba2e935869a3c71a641ef28d
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-21 07:45:02 +0000

    Updated clean script

--- clean.sh
+++ clean.sh
@@ -1,9 +1,2 @@
-#pio run --target clean
-
-SOURCE_FILE="src/TemperatureHumidityDHTSensorFan/TemperatureHumidityDHTSensorFan.ino"
-
-sed -i "s/#define VERSION .*/#define VERSION \"1-0-0-0\"/" $SOURCE_FILE
-
-if [ -d tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/obj ]; then
-  rm tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/obj -R
-fi
+sh inject-version.sh "1-0-0-1" && \
+sh inject-board-type.sh "uno"
