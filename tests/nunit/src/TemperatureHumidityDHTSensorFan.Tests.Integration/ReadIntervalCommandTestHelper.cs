bd7f7b6fcead992d9196b40ba7ff3df80b4b3e13
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-09 18:22:45 +1000

    Trying to fix tests

--- tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/ReadIntervalCommandTestHelper.cs
+++ tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/ReadIntervalCommandTestHelper.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+
 namespace TemperatureHumidityDHTSensorFan.Tests.Integration
 {
-	public class ReadIntervalCommandTestHelper : GreenSenseHardwareTestHelper
-	{
-		public int ReadInterval = 1;
+    public class ReadIntervalCommandTestHelper : GreenSenseHardwareTestHelper
+    {
+        public int ReadInterval = 5;
 
-		public ReadIntervalCommandTestHelper()
-		{
-		}
+        public ReadIntervalCommandTestHelper ()
+        {
+        }
 
-		public void TestSetReadIntervalCommand()
-		{
-			WriteTitleText("Starting read interval command test");
+        public void TestSetReadIntervalCommand ()
+        {
+            WriteTitleText ("Starting read interval command test");
 
-			Console.WriteLine("Read interval: " + ReadInterval);
+            Console.WriteLine ("Read interval: " + ReadInterval);
 
-			ConnectDevices(false);
+            ConnectDevices (false);
 
-			SetDeviceReadInterval(ReadInterval);
+            SetDeviceReadInterval (ReadInterval);
 
-			var dataEntry = WaitForDataEntry();
+            var dataEntry = WaitForDataEntry ();
 
-			AssertDataValueEquals(dataEntry, "I", ReadInterval);
-		}
-	}
+            AssertDataValueEquals (dataEntry, "I", ReadInterval);
+        }
+    }
 }
