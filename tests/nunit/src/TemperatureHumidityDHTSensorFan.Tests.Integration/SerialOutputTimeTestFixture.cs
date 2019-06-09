d2c0a21b76f294a329fee1b4305672b7d48fd764
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-09 19:44:54 +1000

    Fixed tests

--- tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/SerialOutputTimeTestFixture.cs
+++ tests/nunit/src/TemperatureHumidityDHTSensorFan.Tests.Integration/SerialOutputTimeTestFixture.cs
@@ -2,41 +2,39 @@
 
 namespace TemperatureHumidityDHTSensorFan.Tests.Integration
 {
-	[TestFixture(Category = "Integration")]
-	public class SerialOutputTimeTestFixture : BaseTestFixture
-	{
-		[Test]
-		public void Test_SerialOutputTime_3Seconds()
-		{
-			using (var helper = new SerialOutputTimeTestHelper())
-			{
-				helper.ReadInterval = 3;
+    [TestFixture (Category = "Integration")]
+    public class SerialOutputTimeTestFixture : BaseTestFixture
+    {
+        [Test]
+        public void Test_SerialOutputTime_5Seconds ()
+        {
+            using (var helper = new SerialOutputTimeTestHelper ()) {
+                helper.ReadInterval = 5;
 
-				helper.DevicePort = GetDevicePort();
-				helper.DeviceBaudRate = GetDeviceSerialBaudRate();
+                helper.DevicePort = GetDevicePort ();
+                helper.DeviceBaudRate = GetDeviceSerialBaudRate ();
 
-				helper.SimulatorPort = GetSimulatorPort();
-				helper.SimulatorBaudRate = GetSimulatorSerialBaudRate();
+                helper.SimulatorPort = GetSimulatorPort ();
+                helper.SimulatorBaudRate = GetSimulatorSerialBaudRate ();
 
-				helper.TestSerialOutputTime();
-			}
-		}
+                helper.TestSerialOutputTime ();
+            }
+        }
 
-		[Test]
-		public void Test_SerialOutputTime_4Seconds()
-		{
-			using (var helper = new SerialOutputTimeTestHelper())
-			{
-				helper.ReadInterval = 4;
+        [Test]
+        public void Test_SerialOutputTime_6Seconds ()
+        {
+            using (var helper = new SerialOutputTimeTestHelper ()) {
+                helper.ReadInterval = 6;
 
-				helper.DevicePort = GetDevicePort();
-				helper.DeviceBaudRate = GetDeviceSerialBaudRate();
+                helper.DevicePort = GetDevicePort ();
+                helper.DeviceBaudRate = GetDeviceSerialBaudRate ();
 
-				helper.SimulatorPort = GetSimulatorPort();
-				helper.SimulatorBaudRate = GetSimulatorSerialBaudRate();
+                helper.SimulatorPort = GetSimulatorPort ();
+                helper.SimulatorBaudRate = GetSimulatorSerialBaudRate ();
 
-				helper.TestSerialOutputTime();
-			}
-		}
-	}
+                helper.TestSerialOutputTime ();
+            }
+        }
+    }
 }
