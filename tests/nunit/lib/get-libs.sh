1851887c176bfa607be1fe78b340d2d3c84b8727
CompulsiveCoder <compulsivecoder@gmail.com>
2019-07-01 14:40:47 +1000

    Updated ArduinoSerialControllerClient

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -5,6 +5,6 @@ bash install-package-from-libs-repository.sh GreenSense NUnit 2.6.4 || exit 1
 bash install-package-from-libs-repository.sh GreenSense NUnit.Runners 2.6.4 || exit 1
 bash install-package-from-libs-repository.sh GreenSense Newtonsoft.Json 11.0.2 || exit 1
 
-bash install-package-from-github-release.sh CompulsiveCoder ArduinoSerialControllerClient 1.1.1.13 || exit 1
+bash install-package-from-github-release.sh CompulsiveCoder ArduinoSerialControllerClient 1.1.1.14 || exit 1
 
 echo "Finished getting library files."
